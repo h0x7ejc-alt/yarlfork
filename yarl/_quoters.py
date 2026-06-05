01566171c16c746fec6256fa966c95764b5a219a
h0x7ejc-alt <h0x7ejc@gmail.com>
2026-06-05 06:17:50 +0000

    3-3

--- yarl/_quoters.py
+++ yarl/_quoters.py
@@ -4,21 +4,39 @@ from urllib.parse import quote
 
 from ._quoting import _Quoter, _Unquoter
 
+_PATH_SAFE = "@:"
+_PATH_PROTECTED = "/+"
+_QF_SAFE = "?/:@"
+_Q_PROTECTED = "=+&;"
+
+# -- generic ----------------------------------------------------------------
+
 QUOTER = _Quoter(requote=False)
 REQUOTER = _Quoter()
-PATH_QUOTER = _Quoter(safe="@:", protected="/+", requote=False)
-PATH_REQUOTER = _Quoter(safe="@:", protected="/+")
-QUERY_QUOTER = _Quoter(safe="?/:@", protected="=+&;", qs=True, requote=False)
-QUERY_REQUOTER = _Quoter(safe="?/:@", protected="=+&;", qs=True)
-QUERY_PART_QUOTER = _Quoter(safe="?/:@", qs=True, requote=False)
-FRAGMENT_QUOTER = _Quoter(safe="?/:@", requote=False)
-FRAGMENT_REQUOTER = _Quoter(safe="?/:@")
 
-UNQUOTER = _Unquoter()
+# -- path -------------------------------------------------------------------
+
+PATH_QUOTER = _Quoter(safe=_PATH_SAFE, protected=_PATH_PROTECTED, requote=False)
+PATH_REQUOTER = _Quoter(safe=_PATH_SAFE, protected=_PATH_PROTECTED)
 PATH_UNQUOTER = _Unquoter(unsafe="+")
 PATH_SAFE_UNQUOTER = _Unquoter(ignore="/%", unsafe="+")
+
+# -- query ------------------------------------------------------------------
+
+QUERY_QUOTER = _Quoter(safe=_QF_SAFE, protected=_Q_PROTECTED, qs=True, requote=False)
+QUERY_REQUOTER = _Quoter(safe=_QF_SAFE, protected=_Q_PROTECTED, qs=True)
+QUERY_PART_QUOTER = _Quoter(safe=_QF_SAFE, qs=True, requote=False)
 QS_UNQUOTER = _Unquoter(qs=True)
 UNQUOTER_PLUS = _Unquoter(plus=True)  # to match urllib.parse.unquote_plus
+
+# -- fragment ---------------------------------------------------------------
+
+FRAGMENT_QUOTER = _Quoter(safe=_QF_SAFE, requote=False)
+FRAGMENT_REQUOTER = _Quoter(safe=_QF_SAFE)
+
+# -- generic unquoter -------------------------------------------------------
+
+UNQUOTER = _Unquoter()
 
 
 def human_quote(s: str | None, unsafe: str) -> str | None:
